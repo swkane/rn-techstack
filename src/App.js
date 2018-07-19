1b699f88c76866fe0215ff2e2ca1dec5f6a0d32a
swkane <samuelwkane@gmail.com>
2018-07-19 16:57:12 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const App = () => {
+  return (
+    <Provider store={createStore}>
+      <View />
+    </Provider>
+  );
+};
+
+export default App;
